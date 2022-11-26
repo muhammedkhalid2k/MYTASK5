9c12296a1d7d6b0064eec0054095309dd9264f63
muhammedkhalid2k <115889820+muhammedkhalid2k@users.noreply.github.com>
2022-11-26 18:10:46 +0300

    TASK

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,9 +1,7 @@
 class Participant < ApplicationRecord
   has_many :registries
   has_many :coordinators, through: :registries
-  validates :registry, uniqueness: {scope: :coordinator}
-  validates :coordinator, presence: true
-  validates :registry, presence: true
+
   validates :gender, presence: true
   validates :birthday, presence: true
 end
